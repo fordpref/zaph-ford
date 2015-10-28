77a023f6e4d3aba1a871005687201a3008577b99
fordpref <mike@ermanfamily.com>
2015-10-27 22:22:36 -0400

    2015.10.27 Troubleshooting and adding checks
    Just adding some checks on wifi, SSIDs, etc.

--- app/src/main/java/com/ford/zaphod/garagedoor/MainActivity.java
+++ app/src/main/java/com/ford/zaphod/garagedoor/MainActivity.java
@@ -23,43 +23,50 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String status;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButton = (Button)findViewById(R.id.garagebutton);
         //mButton.setBackgroundColor(Color.RED);
         mButton.setOnClickListener(this);
-        if (getWifiName(this).equals("solarsphere")) {
-            mButton.setBackgroundColor(Color.GREEN);
-        } else {
-            mButton.setBackgroundColor(Color.RED);
-        }
+        status = getWifiName(this);
     }
 
     protected void onResume(Bundle savedInstanceState) {
+        String status;
         setContentView(R.layout.activity_main);
         mButton = (Button)findViewById(R.id.garagebutton);
         //mButton.setBackgroundColor(Color.RED);
         mButton.setOnClickListener(this);
-        if (getWifiName(this).equals("solarsphere")) {
-            mButton.setBackgroundColor(Color.GREEN);
-        } else {
-            mButton.setBackgroundColor(Color.RED);
-        }
+        status = getWifiName(this);
     }
 
     public String getWifiName(Context context) {
         android.net.wifi.WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        String ssid = "";
+        String ssid = " ";
         if (manager.isWifiEnabled()) {
             WifiInfo wifiInfo = manager.getConnectionInfo();
-            //if (wifiInfo != null) {
-                //NetworkInfo.DetailedState state = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
+            if (wifiInfo != null) {
+                NetworkInfo.DetailedState state = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
                 //if (state == NetworkInfo.DetailedState.CONNECTED) {
-                    ssid = wifiInfo.getSSID();
-                    //return wifiInfo.getSSID();
+                ssid = wifiInfo.getSSID();
+                ssid = ssid.substring(1, ssid.length() - 1);
+                if (ssid.equalsIgnoreCase("solarsphere")) {
+                    mButton.setBackgroundColor(Color.GREEN);
+                    mButton.setText("Actuate Door\nConnected to Solarsphere");
+                } else {
+                    mButton.setBackgroundColor(Color.RED);
+                    mButton.setText("Door Not Available");
+                }
+                //mButton.setText(ssid);  //Troubleshooting line
+                return ssid;
                 //}
-            //}
-        } return ssid;
+            }
+        } else {
+            mButton.setBackgroundColor(Color.GRAY);
+            mButton.setText("No WIFI\nDoor Not Available");
+        }
+        return ssid;
     }
 
     @Override
